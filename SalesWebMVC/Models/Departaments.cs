cabd245867469d92679a5ce630fa3ddc903eccad
Nicolas Tavares <nicolastavares04@gmail.com>
2025-02-26 21:24:59 -0300

    Fisrt Model-View-Controller - Departament

---
+++ SalesWebMVC/Models/Departaments.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace SalesWebMVC.Models
+{
+    public class Departaments
+    {
+        //Criando nossa Model
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
